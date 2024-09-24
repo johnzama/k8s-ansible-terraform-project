ce3224f6759aa6621eee3144c47d44a1450bba0d
johnzama <johnsparta2121@gmail.com>
2024-09-24 14:53:00 -0400

    32

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "k8s_instance" {
+  ami           = "ami-0dc2d3e4c0f9ebd18"  # Amazon Linux 2 AMI (find the latest for your region)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "K8s-Instance"
+  }
+
+  user_data = <<-EOF
+  #!/bin/bash
+  # Install Docker
+  sudo yum update -y
+  sudo yum install docker -y
+  sudo systemctl start docker
+  sudo systemctl enable docker
+
+  # Install Kubernetes tools
+  sudo curl -Lo /usr/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+  sudo chmod +x /usr/bin/kubectl
+  EOF
+}
+
